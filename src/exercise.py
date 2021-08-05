5beb8c5bcb6ecf1b9f3406228ecb7da55ff1b64e
Justin Turner <jmturner638@gmail.com>
2021-08-05 14:16:25 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,19 @@
 def main():
     #write your code below this line
+    file_name = input("Name of the file:")
+    word = input("Search for:")
+
+    try:
+        with open(file_name, 'r') as f:
+            words = f.read().splitlines()
+
+        if word in words:
+            print("Found!")
+        else:
+            print("Not found.")
+    except:
+        print("Reading the file {} failed.".format(file_name))
+
 
 if __name__ == '__main__':
     main()
